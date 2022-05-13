a7c3312a3613619d07fdf815bb50e68422534f00
Marcos Verblud <marcosverblud@gmail.com>
2022-05-13 17:19:03 -0300

    docs agregado

---
+++ docs/precache-manifest.a0d1e7a9163278187cbb88276456a9af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e6e6716246128f6b99562095c38276d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07a2ca2e1525327e5da9",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07a2ca2e1525327e5da9",
+    "url": "/static/js/main.54cd2623.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
